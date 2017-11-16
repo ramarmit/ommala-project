c69d4fe1b00b314462f8835a91e9f33b1dcbf99f
ramar <ramarmit@gmail.com>
2017-11-17 02:31:19 +0530

    first commit

---
+++ src/main/java/com/example/GitCalc.java
@@ -0,0 +1,16 @@
+package com.example;
+
+public class GitCalc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	int sum(int i, int j){
+		int sum=i+j;
+		System.out.println("Sum is "+ sum);
+      return sum;
+	}
+	
+}
